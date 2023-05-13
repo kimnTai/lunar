7bb6de769d76ee493bd7f28fc36b745ef357dfb7
Hsinny <hsinnyliu@gmail.com>
2023-05-13 16:04:39 +0800

    [ feat ] Card API 新增取得卡片資料

--- src/api/cards.ts
+++ src/api/cards.ts
@@ -1,9 +1,12 @@
 import Request from "@/utils/request";
 import { UpdateCardProps, NewCardProps } from "@/interfaces/cards";
 
+// 取得單張卡片
+export const getCardApi = (cardId: string) => Request.get(`/cards/${cardId}`);
+
 // 新增
 export const newCardApi = (data: NewCardProps) => Request.post("/cards", data);
 
-// 更新card
+// 更新卡片標題、描述、位置、封存
 export const updateCardApi = (data: UpdateCardProps) =>
   Request.put(`/cards/${data.cardId}`, data);
