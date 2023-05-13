de669d99d7e5d0b3e7e99a93a0de7f7c05f9e250
Hsinny <hsinnyliu@gmail.com>
2023-05-13 16:08:42 +0800

    [ feat ] 打開 Card Modal 取得卡片資料

--- src/components/TrelloCard/Modal/index.tsx
+++ src/components/TrelloCard/Modal/index.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { TrelloCardModalStyled } from "./style";
 import { TrelloCardModalProps } from "@/interfaces/trelloCard";
+import { CardsProps } from "@/interfaces/cards";
+import { getCardApi } from "@/api/cards";
 import ModalHeader from "./ModalHeader";
 import ModalLayout from "./ModalLayout";
 
 const TrelloCardModal: React.FC<TrelloCardModalProps> = (props) => {
   const { openModal, setOpenModal } = props;
+
+  const [cardData, setCardData] = useState<CardsProps | null>(null);
+
   const handleOk = () => {
     setOpenModal({ id: "", open: false });
   };
@@ -13,6 +18,22 @@ const TrelloCardModal: React.FC<TrelloCardModalProps> = (props) => {
     setOpenModal({ id: "", open: false });
   };
   return (
+
+  // 打開卡片 Modal 時，取得卡片資料
+  useEffect(() => {
+    if (!openModal.open) return;
+
+    (async function () {
+      try {
+        const { result }: { result: CardsProps } = await getCardApi(
+          openModal.id
+        );
+        setCardData(result);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, [openModal.open]);
     <TrelloCardModalStyled
       open={openModal.open}
       onOk={handleOk}
