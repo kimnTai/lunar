25459844dffb906661a750b21068b6003f673e1f
kimn <a9523023650@gmail.com>
2023-05-19 00:53:09 +0800

    [ fix ] tsc 警告

--- src/context/CardModalContext.tsx
+++ src/context/CardModalContext.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, createContext, useEffect, useState } from "react";
+import React, { ReactNode, createContext, useState } from "react";
 import { CardsProps } from "@/interfaces/cards";
 
 interface CardModalProviderProps {
