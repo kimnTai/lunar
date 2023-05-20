913e37a7465db50f650d7ac029975b5f1f125fea
kimn <a9523023650@gmail.com>
2023-05-20 22:25:47 +0800

    [ feat ] 新增 卡片內 標籤顯示

--- src/components/TrelloCard/Modal/ModalContent/index.tsx
+++ src/components/TrelloCard/Modal/ModalContent/index.tsx
@@ -3,11 +3,13 @@ import { ModalStyle, ModalContentStyled } from "./style";
 import CardDescription from "./CardDescription";
 import CardMember from "./CardMember";
 import CardComment from "./CardComment";
+import CardLabel from "./CardLabel";
 
 const ModalContent: React.FC = () => {
   return (
     <ModalContentStyled style={ModalStyle}>
       <CardMember />
+      <CardLabel />
       <CardDescription />
       <CardComment />
     </ModalContentStyled>
