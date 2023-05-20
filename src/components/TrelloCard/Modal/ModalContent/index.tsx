954f965d70cbcd89d570d90a3efb5d52cfa0618d
kimn <a9523023650@gmail.com>
2023-05-20 23:43:24 +0800

    [ feat ] 新增卡片日期顯示

--- src/components/TrelloCard/Modal/ModalContent/index.tsx
+++ src/components/TrelloCard/Modal/ModalContent/index.tsx
@@ -4,12 +4,14 @@ import CardDescription from "./CardDescription";
 import CardMember from "./CardMember";
 import CardComment from "./CardComment";
 import CardLabel from "./CardLabel";
+import CardDate from "./CardDate";
 
 const ModalContent: React.FC = () => {
   return (
     <ModalContentStyled style={ModalStyle}>
       <CardMember />
       <CardLabel />
+      <CardDate />
       <CardDescription />
       <CardComment />
     </ModalContentStyled>
