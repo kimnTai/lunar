a8ddb9011227b828bcf7cfb30c7cd4b660ba6c65
kimn <a9523023650@gmail.com>
2023-05-20 18:03:11 +0800

    [ feat ] 新增卡片成員顯示

--- src/components/TrelloCard/Modal/ModalContent/index.tsx
+++ src/components/TrelloCard/Modal/ModalContent/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { ModalStyle, ModalContentStyled } from "./style";
 import CardDescription from "./CardDescription";
+import CardMember from "./CardMember";
 
 const ModalContent: React.FC = () => {
   return (
     <ModalContentStyled style={ModalStyle}>
+      <CardMember />
       <CardDescription />
     </ModalContentStyled>
   );
