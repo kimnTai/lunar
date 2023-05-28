42bc5f43fdf5fd722466920ecbedba0851d57008
kimn <a9523023650@gmail.com>
2023-05-28 21:51:00 +0800

    [ fix ] 修正 CardInnerDate 樣式錯誤

--- src/components/TrelloCard/CardInnerDate.tsx
+++ src/components/TrelloCard/CardInnerDate.tsx
@@ -15,34 +15,24 @@ const CardInnerDate: React.FC<{ date: DateProps | null }> = ({ date }) => {
         <div>
           <ClockCircleOutlined />
         </div>
-        {(() => {
-          const startMonth = new Date(date.startDate).getMonth() + 1;
-          const dueMonth = new Date(date.dueDate).getMonth() + 1;
-          const startDay = new Date(date.startDate).getDate();
-          const dueDay = new Date(date.dueDate).getDate();
+        <div style={{ marginLeft: "8px", letterSpacing: "2px" }}>
+          {(() => {
+            const startMonth = new Date(date.startDate).getMonth() + 1;
+            const dueMonth = new Date(date.dueDate).getMonth() + 1;
+            const startDay = new Date(date.startDate).getDate();
+            const dueDay = new Date(date.dueDate).getDate();
 
-          if (date.startDate && date.dueDate) {
-            return (
-              <div style={{ marginLeft: "8px", letterSpacing: "2px" }}>
-                {startMonth}月{startDay}日-{dueMonth}月{dueDay}日
-              </div>
-            );
-          }
+            if (date.startDate && date.dueDate) {
+              return `${startMonth}月${startDay}日-${dueMonth}月${dueDay}日`;
+            }
 
-          if (date.startDate) {
-            return (
-              <div>
-                {startMonth}月{startDay}日
-              </div>
-            );
-          }
+            if (date.startDate) {
+              return `${startMonth}月${startDay}日`;
+            }
 
-          return (
-            <div>
-              {dueMonth}月{dueDay}日
-            </div>
-          );
-        })()}
+            return `${dueMonth}月${dueDay}日`;
+          })()}
+        </div>
       </div>
     )
   );
