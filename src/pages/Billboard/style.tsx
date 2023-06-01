286e83e095eac1c99fca586458ae788641eb44a7
Altuve <chickbooman@hotmail.com>
2023-06-01 15:43:36 +0800

    [ fix ]  修正main 超過長度問題

--- src/pages/Billboard/style.tsx
+++ src/pages/Billboard/style.tsx
@@ -9,7 +9,7 @@ export const BillboardStyled = styled.div`
 
 export const MainLayoutCss = styled(Layout.Content)<{ workspace: string }>`
   transition: all 0.2s ease;
-  // height: calc(100vh - 56px);
+  height: calc(100vh - 72px);
   background-color: ${(props) =>
     props.workspace === "true" ? "white" : "var(--black23)"};
   padding: 24px 48px;
