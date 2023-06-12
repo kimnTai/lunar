0a8484bc9f1bdae3dbc51332f34fe6743b611227
kimn <a9523023650@gmail.com>
2023-06-12 19:05:48 +0800

    [ refactor ] getSocketChange 簡化，被動接收 ws 結果

--- src/utils/cardFunc/getSocketChange.ts
+++ src/utils/cardFunc/getSocketChange.ts
@@ -6,49 +6,30 @@ export function getSocketChange(
   cardList: ListsProps[],
   result: SocketResultProps
 ) {
-  if (!("listId" in result)) {
-    const target = cardList.find((ele) => ele._id === result.id);
+  // 列表
+  if ("card" in result) {
+    const list = result;
+    const target = cardList.filter(({ _id }) => _id !== list._id);
+    target.push(list);
 
-    if (target) {
-      // column 互換
-      target.position = result.position;
-    } else {
-      // column 新增
-      cardList.push(result);
-    }
+    return cloneDeep(target);
+  }
+
+  // 卡片
+  if ("listId" in result) {
+    const card = result;
+    cardList.forEach((list) => {
+      list.card = list.card.filter(({ _id }) => _id !== card._id);
+    });
+
+    cardList
+      .filter(({ _id }) => _id === card.listId)
+      .forEach((list) => {
+        list.card.push(card);
+      });
 
     return cloneDeep(cardList);
   }
 
-  // column 內互換 與 column外互換
-  const originColumn = cardList.find((ele) =>
-    ele.card.find((cardEle) => cardEle.id === result.id)
-  );
-
-  const newData = cloneDeep(result);
-  // 新增
-  if (!originColumn) {
-    cardList.find((ele) => ele.id === result.listId)?.card.push(newData);
-    return cloneDeep(cardList);
-  }
-
-  // 同column 互換
-  if (originColumn.id === result.listId) {
-    const useData = originColumn.card.find((ele) => ele.id === result.id);
-    if (useData && useData.position !== result.position) {
-      useData.position = result.position;
-
-      return cloneDeep(cardList);
-    }
-  }
-
-  // 跨Column 互換
-  if (originColumn.id !== result.listId) {
-    const newColumn = cardList.find((ele) => ele.id === result.listId);
-    originColumn.card = originColumn.card.filter((ele) => ele.id !== result.id);
-    newColumn?.card.push(newData);
-
-    return cloneDeep(cardList);
-  }
   return cardList;
 }
