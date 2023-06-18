9ba7319b3ad58f6a6762ca5ed03ba58da8de17be
Hsinny <hsinnyliu@gmail.com>
2023-06-18 22:35:42 +0800

    [ feat ] 增加右上角頭像內可變更大頭貼功能

--- src/components/User/Header/UserModalButton.tsx
+++ src/components/User/Header/UserModalButton.tsx
@@ -1,17 +1,55 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Avatar, Button, Divider } from "antd";
+import { Avatar, Button, Divider, Spin, Upload } from "antd";
+import { LoadingOutlined, PlusOutlined, UserOutlined } from "@ant-design/icons";
+import ImgCrop from "antd-img-crop";
+import type { UploadChangeParam, UploadFile } from "antd/es/upload/interface";
+import { logout, selectUser, updateProfileAction } from "@/redux/userSlice";
 import { useAppDispatch, useAppSelector } from "@/hooks";
+import openNotification from "@/utils/openNotification";
 import { UserModalCss } from "./style";
-import { logout, selectUser } from "@/redux/userSlice";
 
 const UserModalButton: React.FC = () => {
-  const showWorkSpace = useAppSelector((state) => state.screen.showWorkSpace);
-  const { avatar, name, email } = useAppSelector(selectUser);
-  const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
 
   const dispatch = useAppDispatch();
-  const navigate = useNavigate();
+  const showWorkSpace = useAppSelector((state) => state.screen.showWorkSpace);
+  const { avatar, name, email, _id } = useAppSelector(selectUser);
+
+  const [open, setOpen] = useState(false);
+  const [imgUploading, setImgUploading] = useState<boolean>(false);
+
+  const handleUploadChange = (info: UploadChangeParam<UploadFile<any>>) => {
+    const { file } = info;
+    if (!file) return;
+
+    if (file.status === "uploading") {
+      setImgUploading(true);
+    }
+
+    if (file.status === "error") {
+      setImgUploading(false);
+      openNotification({
+        message: "圖片上傳失敗",
+        success: false,
+      });
+    }
+  };
+
+  const handleAvatarUpload = async (options: { file: any }) => {
+    try {
+      await dispatch(
+        updateProfileAction({
+          file: options.file,
+          userId: _id,
+        })
+      );
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setImgUploading(false);
+    }
+  };
 
   return (
     <>
@@ -35,13 +73,43 @@ const UserModalButton: React.FC = () => {
           {name}
         </p>
       </Button>
+      {/* 大頭照 */}
       <UserModalCss
         open={open}
         onCancel={() => setOpen(false)}
         footer={null}
         width={272}
       >
-        <Avatar src={avatar} style={{ width: "56px", height: "56px" }} />
+        <div className="avatarSection">
+          <ImgCrop>
+            <Upload
+              name="avatar"
+              accept={`.png, .jpg`}
+              onChange={handleUploadChange}
+              customRequest={handleAvatarUpload}
+              showUploadList={false}
+              maxCount={1}
+              className="avatar-uploader"
+            >
+              <div className="avatarBlock">
+                <Avatar
+                  size={64}
+                  src={avatar ? avatar : null}
+                  icon={avatar ? null : <UserOutlined />}
+                  style={imgUploading ? { opacity: 0.3 } : {}}
+                />
+              </div>
+              <Button type="primary" shape="circle" className="avatarUploadBtn">
+                <PlusOutlined className="uploadIcon" />
+              </Button>
+            </Upload>
+          </ImgCrop>
+
+          {imgUploading && (
+            <Spin indicator={<LoadingOutlined spin className="loading" />} />
+          )}
+        </div>
+
         <p style={{ marginTop: "8px", color: "var(--black23)" }}>{name}</p>
         <p style={{ color: "var(--gray9f)" }}>{email}</p>
         <Divider style={{ marginTop: "12px", marginBottom: "12px" }} />
--- src/components/User/Header/style.tsx
+++ src/components/User/Header/style.tsx
@@ -15,6 +15,50 @@ export const UserModalCss = styled(Modal)`
   .ant-modal-body {
     text-align: center;
   }
+
+  .avatarSection {
+    display: inline-block;
+    margin-bottom: 16px;
+    position: relative;
+  }
+
+  .ant-upload {
+    height: 64px;
+    display: inline-block;
+    position: relative;
+  }
+
+  .avatar-uploader {
+    position: relative;
+  }
+
+  .avatarBlock {
+    display: inline-block;
+    position: relative;
+    cursor: pointer;
+  }
+
+  .avatarUploadBtn {
+    width: 24px;
+    height: 24px;
+    min-width: 24px;
+    line-height: 1;
+    position: absolute;
+    right: -4px;
+    bottom: 0px;
+  }
+
+  .uploadIcon {
+    font-size: 10px;
+  }
+
+  .loading {
+    font-size: 24px;
+    position: absolute;
+    top: 35%;
+    left: 35%;
+    z-index: 1;
+  }
 `;
 
 export const SearchCardInputStyled = styled.div`
