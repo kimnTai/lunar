6e2d5a5e017c27aff7f30372d6015ff9a886af61
Altuve <chickbooman@hotmail.com>
2023-06-19 11:59:44 +0800

    [ fix ] cardHeader
    修正 cardHeader 過長跑版問題

--- src/components/TrelloCard/CardInner/CardInnerTitle.tsx
+++ src/components/TrelloCard/CardInner/CardInnerTitle.tsx
@@ -4,12 +4,14 @@ const CardInnerTitle: React.FC<{ name: string }> = ({ name }) => {
   return (
     <div
       style={{
-        width: "256px",
         height: "21px",
         fontSize: "14px",
         fontWeight: "700",
         lineHeight: "150%",
         padding: "0 12px",
+        whiteSpace: "nowrap",
+        overflow: "hidden",
+        textOverflow: "ellipsis",
       }}
     >
       {name}
