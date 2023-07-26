43e2542fa16a0ccc430b2aea1fa03289405ec89c
kimn <a9523023650@gmail.com>
2023-07-26 21:04:53 +0800

    Updates

---
+++ assets/index-50c6260c.js
@@ -0,0 +1,111 @@
+import{am as d,as as s,ai as e,an as x,aH as b,c9 as f,ak as B,ca as v,r as p,cb as h,cc as C,ao as a,cd as S,aD as w,aF as o,ar as r,bB as y,ce as Q,cf as k}from"./index-b0ad1b59.js";import{L as F}from"./img_logo-e8fa72dc.js";const E="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABHNCSVQICAgIfAhkiAAAAhZJREFUOI2Fkk1L1FEUxn/PvTNECYHRQqFgmkYdCV/SsmX1FSJalBTu2rTq5QO0aRNE4C5XtSiI2rSqbQQpmMy08D/qqIkUFhGZ0Lz972nhvPhSeeDCPfc8z3N4zj3iL5HKZlNe+85gdhRkiNXYypPLUbS8E6utSSbTf4Sk7oFdBrkd2IDx1FvpTqFQ+LxL4NiJkwMujj9Iu4jbwsxCCH5oaW4mB+AAurr60i6OZ4TMjE9AtYGvH4CKYSuScC6eSWUHU02B4PVIkhAbxSiX/qnqoRDXRkI50R7KiXarhFM+lA4XZ/Mpw9YlyStMACjdPXjaeZuqN/y1MNvTDs/jfzhwmd6BH8BBgBDXRpwUzrX8ceM/5E2O2WhzgM6fdXLqqee1YpR//L8BAixF+VdAGUBS1oGSDUEYTuwlsImj/lNKOiys1gs+0129thc7ne2/ACQBDFtxMbyv1wyv8a6uvjQ7FqzROZUdTAk9aTyYMSkYTh7vra4p8MywL3LurmFvQ6xbS3O5KYBMpn+IBPeRzrf07PvCbLLTwXSV2K7gdB3TvBnjMqXxbq0BDQltbCeDBUZhuuoAinMfX5txW14TweJJI75UPuC+NcFy5W1mjJvFQv4NrWkSilHuQahx1Ts3Ifl3bZVKR8urqX4pBbOLC1HuIRC2CgCExfncy9rv9Q4zje2XmhYqbf6rWRirlVznYpR/ATSX7Q+5eehSd/lKXQAAAABJRU5ErkJggg==",I="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAHpSURBVHgBpVO9bhNBEJ7Zu4uMSHH8CCkCS+cKCoqNlEgpbdxQIGGgAKpQ0NsFHUVC4TpGPAChChSIo4iEBJavjJCQDvEArGiCsCCH8svZu8Mc8RnfxUmKzBajmZ1v5pudWYScrF+f86xY1wGhxqaX+BAgAqAAQfjn2x9fjMbjqPGjOttiVYcjBZV2RGXq3ZpKLDt1d6uzHQIow7FiXEvHbmrZaeUsmCIiaFkGQsvBaE/rkgBrAZFcEP3KhfdhOORDnYL389nlr+b3xD6UQBnTq0wFocrXXi9LL+/Hftt5bnatBzsfLkL85Sxo3SuNAx8mNleUWNBw+sY3sC9tL08++p4BV5s7dSJyx4ERMUreQKaOguwG+SBC04DBOA/c8RFwQrERQXEbXmK04yKzUZlFAQM+0xi2QIIk/mcdYa/tLG0ap/F06yqs7hW5Jz29dt9Xh1WsNreG+8KFAxHG597Ob5Rh9U8x2Uu3j+LN3ErNGwe+1txcHN0XTrD8b5VnXt1eYpqN9AKBFAhskcbPaDDSQkuBOD+xcatc+FXbB3OvnceTpeFfmHl5x2d1E44Ra/cKnOo+jGDbnQ6enFGZz8RMFpjJ4lEJeHRKk6mEg3fCfIBcuedZ2Ev2XnIyCQO6PK2Qtf/p7uvMlP4C50DDuU1MO7EAAAAASUVORK5CYII=",U=""+new URL("bg_createAccount-b0f63f4c.png",import.meta.url).href,X=d.div`
+  position: relative;
+  background-image: url(${U});
+  background-size: cover;
+  background-position: center;
+  height: 100%;
+  width: 100%;
+
+  .header {
+    position: absolute;
+    left: 21.15%;
+    top: 28px;
+    z-index: 5;
+  }
+  .ant-card {
+    position: absolute;
+    width: 448px;
+    font-size: 14px;
+    top: calc(50% - 614px / 2);
+    left: calc(67.18% - 448px / 2);
+    z-index: 6;
+    @media (max-width: 768px) {
+      left: 50%;
+      transform: translateX(-50%);
+    }
+  }
+  .ant-card-body {
+    padding: "40px 64px 32px 64px";
+  }
+  .cardHeader {
+    margin-bottom: 24px;
+    font-size: 28px;
+    font-weight: 700;
+    text-align: center;
+    width: 100%;
+  }
+  .ant-form-item {
+    margin-bottom: 12px;
+    input {
+      height: 48px;
+      font-size: 16px;
+    }
+  }
+  .terms {
+    color: var(--gray9f);
+    text-align: center;
+  }
+  .have-account {
+    margin-top: 24px;
+    text-align: center;
+    color: var(--gray66);
+  }
+
+  .contentText {
+    position: absolute;
+    left: 26.04%;
+    top: 50%;
+    z-index: 5;
+
+    h3 {
+      font-size: 32px;
+      font-weight: 700;
+    }
+    span {
+      font-size: 16px;
+      display: block;
+    }
+  }
+
+  .background {
+    width: 100%;
+    height: 100%;
+    position: relative;
+    overflow: hidden;
+    .red_ball {
+      position: absolute;
+      left: 45.31%;
+      top: 27.685%;
+      object-fit: cover;
+    }
+    .blue_ball {
+      position: absolute;
+      top: 52.5%;
+      left: 39.895%;
+      object-fit: cover;
+    }
+    .bg_blue {
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      object-fit: cover;
+    }
+    .bg_gray {
+      position: absolute;
+      top: 0;
+      right: 0;
+      object-fit: cover;
+    }
+  }
+`,N=d(s)`
+  width: 100%;
+  font-size: 14px;
+  margin-top: 8px;
+  height: 44px;
+  img {
+    margin-right: 8px;
+  }
+  span {
+    width: 144px;
+  }
+`,A=({iconSrc:n,...i})=>e(N,{...i,icon:e("img",{src:n,alt:""}),className:"d-center",children:i.text}),D=()=>{const n=x(),i=b(),t=f().pathname==="/signup",c=B(v),[g,l]=p.useState(!1);return p.useEffect(()=>{c&&(n(h(!0)),n(C()).then(()=>{i("/")}).finally(()=>{n(h(!1))}))},[c]),a(X,{children:[e(S,{}),e("img",{className:"header",src:F,alt:""}),a(w,{children:[e("h1",{className:"cardHeader",children:t?"免費註冊":"登入"}),a(o,{name:"login-form",wrapperCol:{span:24},onFinish:async u=>{l(!0);const m=t?Q:k;try{await n(m(u)).unwrap(),i("/")}catch{}l(!1)},children:[e(o.Item,{name:"email",rules:[{required:!0,message:"請輸入您的 Email!",type:"email"}],children:e(r,{placeholder:"Email"})}),t&&e(o.Item,{name:"name",rules:[{required:!0,message:"請輸入您的帳號!"}],children:e(r,{placeholder:"帳號"})}),e(o.Item,{name:"password",rules:[{required:!0,message:"請輸入您的密碼!"}],children:e(r,{placeholder:"密碼",type:"password"})}),e(o.Item,{children:e(s,{loading:g,type:"primary",htmlType:"submit",style:{width:"100%",height:"48px",fontWeight:700},children:t?"註冊":"登入"})})]}),a("div",{className:"terms",style:{display:t?"block":"none"},children:[e("div",{children:"點擊註冊表示您同意我們的"}),a("div",{children:[e("a",{href:"https://policies.google.com/privacy",children:"隱私政策"}),e("span",{style:{margin:"0 8px"},children:"和"}),e("a",{href:"https://policies.google.com/terms",children:"服務條款"})]})]}),e(y,{plain:!0,style:{borderColor:"#D4D4D4",margin:"12px 0px",height:"20px"},children:"或"}),e(A,{iconSrc:I,text:t?"使用 Google 註冊":"使用 Google 登入",onClick:()=>{window.location.href="https://prometheus2023.zeabur.app/api/user/google"}}),e(A,{iconSrc:E,text:t?"使用 GitHub 註冊":"使用 GitHub 登入",onClick:()=>{window.location.href="https://prometheus2023.zeabur.app/api/user/github"}}),a("div",{className:"have-account",children:[e("div",{children:"已經有帳戶了嗎？"}),e("div",{children:e(s,{type:"link",style:{fontSize:"16px",padding:"0"},onClick:()=>i(t?"/login":"/signup"),children:t?"登入":"註冊"})})]})]}),a("div",{className:"contentText",children:[e("h3",{children:"讓工作，更有序"}),e("span",{children:"Simplify work and boost results."})]})]})};export{D as default};
