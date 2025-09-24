e10301e4fc6c675e3a2c1bd5e939063a601e2c4b
ptp_ting-xuan <ptp_ting-xuan@photons.com.tw>
2025-09-25 01:51:19 +0800

    [ modify ] 首頁跳轉 login

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,22 +1,10 @@
-import { useEffect } from "react";
-import Footer from "./Footer";
-import Header from "./Header";
-import HomeContent from "./HomeContent";
+import { Navigate } from "react-router";
 
 export default function Home() {
-  useEffect(() => {
-    if (import.meta.env.PROD) {
-      window.location.href = `https://lunar-sigma.vercel.app/`;
-    }
-  }, []);
+  if (import.meta.env.PROD) {
+    window.location.href = "https://lunar-sigma.vercel.app/";
+    return null;
+  }
 
-  return import.meta.env.PROD ? (
-    <></>
-  ) : (
-    <>
-      <Header />
-      <HomeContent />
-      <Footer />
-    </>
-  );
+  return <Navigate to="/login" />;
 }
